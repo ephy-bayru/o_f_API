49a8fa23621dabd0a00d791de2f94580584ac662
Ephrem <ephybayru@gmail.com>
2023-05-09 15:06:30 +0300

    feat: add authentication and authorization to endpoints in authRouter

---
+++ src/components/auth/v1/web/auth.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { AuthController } from './auth.controller';
+import { Permission } from '../../../../config/permissions';
+import { authenticate } from '../middlewares/authenticate';
+import { authorize } from '../middlewares/authorize';
+
+const authController = new AuthController();
+
+const authRouter = Router();
+
+// No authentication or authorization needed for login and register
+authRouter.post('/login', (req, res) => authController.login(req, res));
+authRouter.post('/register', (req, res) => authController.register(req, res));
+
+// Authentication is needed for refreshing token, resetting password, and changing password
+authRouter.post('/refreshToken', authenticate, (req, res) => authController.refreshToken(req, res));
+authRouter.post('/resetPassword', authenticate, (req, res) => authController.resetPassword(req, res));
+authRouter.put('/changePassword', authenticate, (req, res) => authController.changePassword(req, res));
+
+// Both authentication and authorization needed for suspending user
+authRouter.put('/suspendUser/:userId', authenticate, authorize(Permission.SUSPEND_USER), (req, res) => authController.suspendUser(req, res));
+
+export default authRouter;
