6d9a451f00a2ad1d2f6a0bdc6bda18861730ff72
Ephrem <ephybayru@gmail.com>
2023-05-10 22:40:43 +0300

    feat: Implement rate limiting for login endpoint
    - Imported node-rate-limiter-flexible and used it to create a MongoDB rate limiter for the login route.
    - Updated auth routes to apply the rate limiter to the login endpoint.

--- src/components/auth/v1/web/auth.routes.ts
+++ src/components/auth/v1/web/auth.routes.ts
@@ -3,13 +3,14 @@ import { AuthController } from './auth.controller';
 import { Permission } from '../../../../config/permissions';
 import { authenticate } from '../middlewares/authenticate';
 import { authorize } from '../middlewares/authorize';
+import { loginRateLimiter } from '../../../../middlewares/rateLimit.middleware';
 
 const authController = new AuthController();
 
 const authRouter = Router();
 
 // No authentication or authorization needed for login and register
-authRouter.post('/login', (req, res) => authController.login(req, res));
+authRouter.post('/login', loginRateLimiter, (req, res) => authController.login(req, res));
 authRouter.post('/register', (req, res) => authController.register(req, res));
 
 // Authentication is needed for refreshing token, resetting password, and changing password
